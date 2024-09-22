45904f8a656a2ddb620802891afca952b29ff67c
Ivan <ivan.ho92@hotmail.com>
2024-09-23 00:59:12 +0800

    Deployed storage and Azure function

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounterivan.azurewebsites.net/api/GetResumeCounter?code=4RTmXqmDxC8q2owzmYvOswCrh2J2tzUqFMIx-83k94-cAzFuwn4FZA%3D%3D';
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.");
